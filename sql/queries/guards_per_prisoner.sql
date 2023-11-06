7168b7c6c978ea09e172c1e9ba15cc49a1af17ee
Maja <maja.placek451@gmail.com>
2023-11-06 23:21:15 +0100

    fixed queries

--- sql/queries/guards_per_prisoner.sql
+++ sql/queries/guards_per_prisoner.sql
@@ -1,15 +1,19 @@
+WITH PrisonStats AS (
+  SELECT
+    P."PenitentiaryName" AS "PrisonName",
+    COUNT(DISTINCT Pz."IdPrisoner") AS "NumberOfPrisoners",
+    COUNT(DISTINCT G."IdGuard") AS "NumberOfGuards"
+  FROM
+    public."Prison" P
+    LEFT JOIN public."Prisoner" Pz ON P."IdPrison" = Pz."IdPrison"
+    LEFT JOIN public."Guard" G ON P."IdPrison" = G."IdPrison"
+  GROUP BY
+    P."PenitentiaryName"
+)
 SELECT
-    p."PenitentiaryName" AS "PrisonName",
-    COUNT(g."IdGuard") AS "GuardCount",
-    COUNT(pri."IdPrisoner") AS "PrisonerCount",
-    COUNT(g."IdGuard") / COUNT(pri."IdPrisoner") AS "GuardsPerPrisoner"
-FROM
-    public."Prison" p
-LEFT JOIN
-    public."Guard" g ON p."IdPrison" = g."IdPrison"
-LEFT JOIN
-    public."Prisoner" pri ON g."IdPrison" = pri."IdPrisoner"
-GROUP BY
-    "PrisonName"
-ORDER BY
-    "PrisonName";
+  "PrisonName",
+  "NumberOfPrisoners",
+  "NumberOfGuards",
+  "NumberOfGuards"::numeric / NULLIF("NumberOfPrisoners", 0) AS "NumberOfGuardsPerPrisoner"
+FROM PrisonStats
+ORDER BY "PrisonName";
