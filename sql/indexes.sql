d9aed3c0b30a3667a779baf25db0f13e7a698466
Iza <iza.kalenik@gmail.com>
2023-11-09 00:25:27 +0100

    indexes checked

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,16 +1,19 @@
 CREATE INDEX idx_prisoner_names ON "Prisoner" ("FirstName", "LastName");
-CREATE INDEX idx_prison_penitentiary ON  "Prison" ("PenitentiaryName");
+CREATE INDEX idx_prison_penitentiary ON "Prison" ("PenitentiaryName");
 CREATE INDEX idx_prisoner_pesel ON "Prisoner" ("PESEL");
 CREATE INDEX idx_specialization ON "Specialization" ("Specialization");
 
 CREATE INDEX idx_doctor_idspecialization ON "Doctor" ("IdSpecialization");
 
-CREATE INDEX idx_examination_type ON "Examination"("ExaminationType");
+-- CREATE INDEX idx_examination_type ON "Examination" ("ExaminationType"); -- będzie poprawiane
 
-CREATE INDEX idx_prisoner_ ON "Prisoner" ("IdPrison");
-CREATE INDEX idx_guard_idprison ON public."Guard" ("IdPrison");
+CREATE INDEX idx_prisoner_idprison  ON "Prisoner" ("IdPrison");
+CREATE INDEX idx_guard_idprison ON "Guard" ("IdPrison");
 
-CREATE INDEX idx_duty_startdate ON public."Duty" ("StartDate");
-CREATE INDEX idx_duty_enddate ON public."Duty" ("EndDate");
+CREATE INDEX idx_duty_startdate ON "Duty" ("StartDate"); -- w moich kwerendach nie zmieniło nic
+CREATE INDEX idx_duty_enddate ON "Duty" ("EndDate");    -- w moich kwerendach nie zmieniło nic
 
+CREATE INDEX idx_guardduty_idduty ON "GuardDuty" ("IdDuty"); --sprawdzone
+CREATE INDEX idx_examination_idprisoner ON "Examination" ("IdPrisoner"); --sprawdzone
+-- Iza: u mnie sprawdzone klucze obce, są tylko dwa powyższe
 
