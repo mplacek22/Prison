6153676577a1c99e1f680f2f13273800d1ac60b1
Iza <iza.kalenik@gmail.com>
2023-11-14 12:25:40 +0100

    deleting comments

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -10,12 +10,11 @@ CREATE INDEX idx_examination_type ON "ExaminationType" ("ExaminationType");
 CREATE INDEX idx_prisoner_idprison  ON "Prisoner" ("IdPrison");
 CREATE INDEX idx_guard_idprison ON "Guard" ("IdPrison");
 
-CREATE INDEX idx_duty_startdate ON "Duty" ("StartDate"); -- w moich kwerendach nie zmieniło nic
-CREATE INDEX idx_duty_enddate ON "Duty" ("EndDate");    -- w moich kwerendach nie zmieniło nic
+CREATE INDEX idx_duty_startdate ON "Duty" ("StartDate");
+CREATE INDEX idx_duty_enddate ON "Duty" ("EndDate");
 
-CREATE INDEX idx_guardduty_idduty ON "GuardDuty" ("IdDuty"); --sprawdzone
-CREATE INDEX idx_examination_idprisoner ON "Examination" ("IdPrisoner"); --sprawdzone
--- Iza: u mnie sprawdzone klucze obce, są tylko dwa powyższe
+CREATE INDEX idx_guardduty_idduty ON "GuardDuty" ("IdDuty");
+CREATE INDEX idx_examination_idprisoner ON "Examination" ("IdPrisoner");
 
 CREATE INDEX idx_guard_idrank ON public."Guard" USING hash ("IdRank");
 
