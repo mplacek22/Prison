97da73ee6842aa65d58dcc64df7c7eb20984430a
izabela <iza.kalenik@gmail.com>
2024-01-13 17:24:01 +0100

    change names

--- non_relational_database/database/database_connection.py
+++ non_relational_database/database/database_connection.py
@@ -3,5 +3,5 @@ import pymongo
 
 def database_connection():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = myclient["Prison2"]
+    db = myclient["Prison"]
     return db
